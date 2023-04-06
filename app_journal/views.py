0d4fb862c9445a1f42327bec4fac49cb9c144611
Endang Ismaya <endang.ismaya@gmail.com>
2023-04-06 11:09:45 +0700

    first migrate

--- app_journal/views.py
+++ app_journal/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
@@ -8,4 +7,4 @@ def register(request):
 
 
 def home(request):
-    return HttpResponse("This is home", {})
+    return render(request, "index.html")

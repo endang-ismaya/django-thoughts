c1aa60207d5575914b8450038bff267f03918008
Endang Ismaya <endang.ismaya@gmail.com>
2023-04-06 11:29:49 +0700

    Create Model Task and Review

--- app_journal/admin.py
+++ app_journal/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from app_journal.models import Task, Review
 
-# Register your models here.
+admin.site.register(Task)
+admin.site.register(Review)
--- app_journal/models.py
+++ app_journal/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Review(models.Model):
+    reviewer_name = models.CharField(max_length=65)
+    review_title = models.CharField(max_length=100)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="review_task")
+
+    def __str__(self) -> str:
+        return self.review_title
